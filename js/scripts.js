a899a4b14aabb12be2cc4bea26f2956c5da777c2
Mat Sadler <mat@redstudio.co.uk>
2012-10-23 14:02:56 +0100

    added .com and co.uk to screen keyboard

--- js/scripts.js
+++ js/scripts.js
@@ -29,15 +29,15 @@ $(document).ready(function() {
 				 customLayout: {
         'default': [
             '` 1 2 3 4 5 6 7 8 9 0 - = {bksp}',
-            'q w e r t y u i o p [ ]',
-            'a s d f g h j k l @ ; ',
+            'q w e r t y u i o p [ ] .com',
+            'a s d f g h j k l @ ; .co.uk',
             '{shift}  z x c v b n m , . {shift}',
             '{accept} {space}  {cancel}'
             ],
             'shift': [
             '` ! " # $ % ^ & * ( ) _ + {bksp}',
-            'Q W E R T Y U I O P [ ]',
-            'A S D F G H J K L @ ; ',
+            'Q W E R T Y U I O P [ ] .com',
+            'A S D F G H J K L @ ; .co.uk',
             '{shift}  Z X C V B N M , . {shift}',
             '{accept} {space}  {cancel}'
             ]
