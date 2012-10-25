750b318dfaa1d04f46d79393743bc24901144636
Mat Sadler <mat@redstudio.co.uk>
2012-10-25 23:39:48 +0100

    some amends. lists recent reviews on front
    added terms

--- application/views/global/terms.php
+++ application/views/global/terms.php
@@ -1,9 +1,56 @@
 <div id="terms" class="popupBack">
 	<div class="popup">
-	<div class="boxPadding">
-		<div class="closePopup"></div>
-		terms to go here
+		<div class="boxPadding">
+			<div class="closePopup"></div>
+			<h2>Terms and Conditions</h2>
+			<ol style="padding-top:30px; padding-left:90px; padding-right:90px; font-size:18px; line-height:25px;" class="orderedlist">
+			<li>
+					The competition is open to all UK residents aged 18 or over, except for anyone directly connected with the planning or administration of the competition.
+			</li>
+			<li>
+					Enter by completing a rating of a wine and entry form via the digital screen. Entrants can enter more than once.
+			</li>
+			<li>
+				 	Entries will be entered into the draw at the end of London Tesco Wine Fair and one winner will be randomly selected and will be contacted by email week commencing 29/10/2012.
+			</li>
+			<li>
+					Only one prize per household.
+			</li>
+			<li>
+				The entry instructions form part of the rules. By entering, entrants agree to accept and be bound by the rules.
+			</li>
+			<li>
+					Ratings will be used on tesco.com/wine
+			</li>
+			<li>
+					The prize is a 6 bottle case of the entrant’s favourite wine tasted at the fair. There will be one prize draw for the whole London fair.
+			</li>
+			<li>
+					All prizes are subject to availability. Where a product is no longer available an equivalent product of the same value will be substituted. There are no cash alternatives.
+			</li>
+			<li>
+					Winners will be notified via email with the address given on the entry form.
+			</li>
+			<li>
+					Winners names will be available after week commencing 5/11/2012 by emailing info@tescowinefair.com
+			</li>
+			<li>
+					Winners agree that their names and photographs may be used for promotional purposes.
+			</li>
+			<li>
+					Promoter: Tesco Stores Ltd, Tesco House, Delamare Road, Cheshunt EN8 9SL.
+			</li>
+			<li>
+					By entering the competition you agree to receive offers and information about Tesco products and services.
+			</li>
+			<li>
+				Copies of these Terms and Conditions are also available on request.
+			</li>
+			</ol>
+			<div style="width:400px;" class="buttonContainer">
+			<div class="acceptTerms buttonBevel">Accept</div>
+			</div>
+		</div>
 	</div>
-	</div>
-	
+
 </div>
--- js/scripts.js
+++ js/scripts.js
@@ -18,8 +18,7 @@ var tips = $( ".validateTips" );
          function checkLength( o, n, min, max ) {
             if ( o.val().length > max || o.val().length < min ) {
                 o.addClass( "ui-state-error" );
-              updateTips( "Length of " + n + " must be between " +
-                  min + " and " + max + "." );
+              updateTips( "Please enter your name");
                 return false;
             } else {
                 return true;
@@ -28,7 +27,7 @@ var tips = $( ".validateTips" );
         
         function checkCheckbox(o){
         	if(o.val() != 1) {
-        		updateTips("You must read the Terms");
+        		updateTips("Please read the terms and conditions");
         		return false; 
         	} else {
         		return true
@@ -129,6 +128,10 @@ $(document).ready(function() {
 	});
 	
 	$('#finish').click(function(){
+			
+	 window.location = base_url;
+	});
+	$('#reset').click(function(){
 			
 	 window.location = base_url;
 	});
@@ -259,7 +262,7 @@ var LastReview = $("#lastReview")
 	var bValid = true;
 	
 	 bValid = bValid && checkLength( name, "Name", 1, 50 );	
-	 bValid = bValid && checkRegexp( email, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, "Must be a valid email eg. name@domain.com" );
+	 bValid = bValid && checkRegexp( email, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, "Please enter a valid email eg. name@domain.com" );
  bValid = bValid && checkCheckbox(checked);	
 		if(bValid) {
 			//form is valid, post it
@@ -309,6 +312,13 @@ var LastReview = $("#lastReview")
 		
 	});
 	
+	$('.acceptTerms').click(function(){
+		$("#termsCheck").val("1");
+		$('.formCheckbox').addClass('isTicked');
+		$("#terms").fadeOut();
+		
+			
+	});
 	
 	$('.formCheckbox').click(function() {
 		
